621cbc4ffcf830a9a33a706ac8f7c94630cdffbc
MrLuffyChan <imluffychan@gmail.com>
2024-12-29 18:18:54 +0530

    Update pmpermit.py

--- Barath/database/pmpermit.py
+++ Barath/database/pmpermit.py
@@ -18,8 +18,8 @@ LIMIT = 5
 async def set_pm(value: bool):
     doc = {"_id": 1, "pmpermit": value}
     doc2 = {"_id": "Approved", "users": []}
-    r = await collection.find_one({"_id": 1})
-    r2 = await collection.find_one({"_id": "Approved"})
+    r = collection.find_one({"_id": 1})
+    r2 = collection.find_one({"_id": "Approved"})
     if r:
         await collection.update_one({"_id": 1}, {"$set": {"pmpermit": value}})
     else:
@@ -29,19 +29,19 @@ async def set_pm(value: bool):
 
 
 async def set_permit_message(text):
-    await collection.update_one({"_id": 1}, {"$set": {"pmpermit_message": text}})
+    collection.update_one({"_id": 1}, {"$set": {"pmpermit_message": text}})
 
 
 async def set_block_message(text):
-    await collection.update_one({"_id": 1}, {"$set": {"block_message": text}})
+    collection.update_one({"_id": 1}, {"$set": {"block_message": text}})
 
 
 async def set_limit(limit):
-    await collection.update_one({"_id": 1}, {"$set": {"limit": limit}})
+    collection.update_one({"_id": 1}, {"$set": {"limit": limit}})
 
 
 async def get_pm_settings():
-    result = await collection.find_one({"_id": 1})
+    result = collection.find_one({"_id": 1})
     if not result:
         return False
     pmpermit = result["pmpermit"]
@@ -53,7 +53,7 @@ async def get_pm_settings():
 
 async def allow_user(chat):
     doc = {"_id": "Approved", "users": [chat]}
-    r = await collection.find_one({"_id": "Approved"})
+    r = collection.find_one({"_id": "Approved"})
     if r:
         await collection.update_one({"_id": "Approved"}, {"$push": {"users": chat}})
     else:
@@ -61,7 +61,7 @@ async def allow_user(chat):
 
 
 async def get_approved_users():
-    results = await collection.find_one({"_id": "Approved"})
+    results = collection.find_one({"_id": "Approved"})
     if results:
         return results["users"]
     else:
@@ -69,11 +69,11 @@ async def get_approved_users():
 
 
 async def deny_user(chat):
-    await collection.update_one({"_id": "Approved"}, {"$pull": {"users": chat}})
+    collection.update_one({"_id": "Approved"}, {"$pull": {"users": chat}})
 
 
 async def pm_guard():
-    result = await collection.find_one({"_id": 1})
+    result = collection.find_one({"_id": 1})
     if not result:
         return False
     if not result["pmpermit"]:
